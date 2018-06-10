b54c2f3af0cb2c3a8c4582167885345ea471000c
Sheharyar Naseer <hello@sheharyar.me>
2018-06-10 15:07:12 +0500

    Cleanup Mix

--- mix.exs
+++ mix.exs
@@ -1,6 +1,7 @@
 defmodule ZTD.Mixfile do
   use Mix.Project
 
+
   def project do
     [
       app: :ztd,
@@ -14,9 +15,7 @@ defmodule ZTD.Mixfile do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
+
   def application do
     [
       mod: {ZTD.Application, []},
@@ -24,32 +23,25 @@ defmodule ZTD.Mixfile do
     ]
   end
 
-  # Specifies which paths to compile per environment.
+
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_),     do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
+
   defp deps do
     [
-      {:phoenix, "~> 1.3.2"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.10"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:phoenix,              "~> 1.3.2"},
+      {:phoenix_pubsub,       "~> 1.0"},
+      {:phoenix_ecto,         "~> 3.2"},
+      {:postgrex,             ">= 0.0.0"},
+      {:gettext,              "~> 0.11"},
+      {:cowboy,               "~> 1.0"}
+      {:phoenix_html,         "~> 2.10"},
+      {:phoenix_live_reload,  "~> 1.0", only: :dev},
     ]
   end
 
-  # Aliases are shortcuts or tasks specific to the current project.
-  # For example, to create, migrate and run the seeds file at once:
-  #
-  #     $ mix ecto.setup
-  #
-  # See the documentation for `Mix` for more info on aliases.
+
   defp aliases do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
