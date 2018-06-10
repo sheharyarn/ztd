bf83b45a9d8b5ce1b76b7193f215f642ed1deca5
Sheharyar Naseer <hello@sheharyar.me>
2018-06-11 03:31:35 +0500

    Use custom namespacing for controllers

--- lib/ztd_web/controllers/page_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule ZTD.Web.PageController do
-  use ZTD.Web, :controller
-
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-end
---
+++ lib/ztd_web/controllers/todo.ex
@@ -0,0 +1,12 @@
+defmodule ZTD.Web.Controllers.Todo do
+  use ZTD.Web, :controller
+
+  alias ZTD.Todo
+
+
+  @doc "GET: All Todo Items"
+  def index(conn, _params) do
+    render(conn, "index.html", items: Todo.all)
+  end
+
+end
