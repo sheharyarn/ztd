a2de78ce5c8af2eaf38a1309c0b0a466e58a546f
Sheharyar Naseer <hello@sheharyar.me>
2018-06-12 01:15:47 +0500

    Set up the basic structure for the Todo component

--- assets/js/components/todo/index.js
+++ assets/js/components/todo/index.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React     from 'react'
+import PropTypes from 'prop-types'
 
 class Todo extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      items: this.props.items,
+    };
   }
 
   render() {
+    const {items} = this.state;
+
     return (
-      <h1>HELLLO!!!</h1>
+      <div className='todo-app'>
+        <todo-input />
+
+        <div className='item-list'>
+          { items.map(i =>
+            <todo-item key={i.id} item={i} />
+          )}
+        </div>
+      </div>
     );
   }
 }
+
+
+// Prop Specification
+
+Todo.defaultProps = {
+  items: [],
+};
+
+Todo.propTypes = {
+  items: PropTypes.array.isRequired,
+};
 
 
 export default Todo;
