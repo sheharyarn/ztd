66b1569d4091674771c8dc4ee7d93b6eb48c5446
Sheharyar Naseer <hello@sheharyar.me>
2018-06-12 09:20:33 +0500

    Tests for handling inserts

--- test/ztd_web/channels/todo_events_test.exs
+++ test/ztd_web/channels/todo_events_test.exs
@@ -1,9 +1,12 @@
 defmodule ZTD.Tests.Web.Channels.TodoEvents do
   use ZTD.Tests.Support.ChannelCase
 
+  alias ZTD.Todo.Item
+  alias ZTD.Web.Channels.TodoEvents
 
-  @channel ZTD.Web.Channels.TodoEvents
-  @room "todo_events"
+
+  @channel  TodoEvents
+  @room     "todo_events"
 
 
   setup do
@@ -13,6 +16,22 @@ defmodule ZTD.Tests.Web.Channels.TodoEvents do
       |> subscribe_and_join(@channel, @room)
 
     [socket: socket]
+  end
+
+
+
+  describe "#handle_in" do
+    @item  %{title: "get milk"}
+    @event "insert"
+    test "inserts new items on insert event", %{socket: socket} do
+      assert length(Item.all) == 0
+
+      push(socket, @event, %{data: @item})
+      Support.wait
+
+      assert [item] = Item.all
+      assert item.title == @item.title
+    end
   end
 
 end
