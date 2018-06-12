5c6a38552380a9f5324ce2bf9b4bcc4b4eb99efb
Sheharyar Naseer <hello@sheharyar.me>
2018-06-12 09:34:12 +0500

    Tests for handling updates and deletes

--- test/ztd_web/channels/todo_events_test.exs
+++ test/ztd_web/channels/todo_events_test.exs
@@ -21,16 +21,43 @@ defmodule ZTD.Tests.Web.Channels.TodoEvents do
 
 
   describe "#handle_in" do
-    @item  %{title: "get milk"}
     @event "insert"
     test "inserts new items on insert event", %{socket: socket} do
       assert length(Item.all) == 0
 
-      push(socket, @event, %{data: @item})
+      new = %{title: "get milk"}
+      push(socket, @event, %{data: new})
       Support.wait
 
       assert [item] = Item.all
-      assert item.title == @item.title
+      assert item.title == new.title
+    end
+
+
+    @event "update"
+    test "updates existing items", %{socket: socket} do
+      old = Item.insert!(title: "done item", done: true)
+      new = %{id: old.id, title: "pending", done: false}
+
+      push(socket, @event, %{data: new})
+      Support.wait
+
+      assert item = Item.get(old.id)
+      assert item.title == new.title
+      assert item.done == new.done
+    end
+
+
+    @event "delete"
+    test "deletes existing items", %{socket: socket} do
+      item_1 = Item.insert!(title: "done item", done: true)
+      item_2 = Item.insert!(title: "pending item", done: false)
+
+      push(socket, @event, %{data: %{id: item_1.id}})
+      Support.wait
+
+      refute Item.get(item_1.id)
+      assert Item.get(item_2.id)
     end
   end
 
