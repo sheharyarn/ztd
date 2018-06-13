e3639f0ee1f1fb4d99e84974c6101ea20028a0a1
Sheharyar Naseer <hello@sheharyar.me>
2018-06-13 16:01:01 +0500

    Move main functionality into Engine namespace

---
+++ lib/ztd/todo/engine/engine.ex
@@ -0,0 +1,54 @@
+defmodule ZTD.Todo.Engine do
+  alias ZTD.Repo
+  alias ZTD.Todo.Engine.Item
+  alias Ecto.Query
+
+  require Query
+
+
+  @moduledoc """
+  Provides an interface to add, update, delete and
+  mark items done/undone.
+  """
+
+
+
+  @doc "Get all todos"
+  def all do
+    Item
+    |> Query.order_by(asc: :inserted_at)
+    |> Repo.all
+  end
+
+
+
+  @doc "Insert new todo"
+  def insert(%{} = params) do
+    Item.insert(params)
+  end
+
+
+
+  @doc "Update a todo"
+  def update(id, %{} = params) do
+    id
+    |> Item.get!
+    |> Item.update(params)
+  end
+
+
+
+  @doc """
+  Delete a todo
+
+  Using a where clause so it doesn't raise errors for
+  stale structs
+  """
+  def delete(id) do
+    Item
+    |> Query.where([i], i.id == ^id)
+    |> Repo.delete_all
+    :ok
+  end
+
+end
--- lib/ztd/todo/engine/item.ex
+++ lib/ztd/todo/engine/item.ex
@@ -1,4 +1,4 @@
-defmodule ZTD.Todo.Item do
+defmodule ZTD.Todo.Engine.Item do
   use ZTD.Repo.Schema
 
 
