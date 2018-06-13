bbfc55e8fd1baf01746995bf67fb8a32bf3fd9a9
Sheharyar Naseer <hello@sheharyar.me>
2018-06-13 15:43:13 +0500

    Configure mode adapters

--- config/config.exs
+++ config/config.exs
@@ -1,28 +1,35 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# General application configuration
+
+# General Application Config
 config :ztd,
   namespace: ZTD,
   ecto_repos: [ZTD.Repo],
   generators: [binary_id: true]
 
-# Configures the endpoint
+
+# Mode Settings / Adapters
+config :ztd, :mode,
+  env_var: "MODE",
+  adapters: [
+    engine: ZTD.Todo.Engine,
+    worker: ZTD.Todo.Worker
+  ]
+
+
+# Endpoint
 config :ztd, ZTD.Web.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "Q6jgvP5PBpizrwNmRpmL+jufEpjbTV9JjEH+FC6HngbYlk2EThFD2CHKSAZ85jOy",
   render_errors: [view: ZTD.Web.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: ZTD.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: ZTD.PubSub, adapter: Phoenix.PubSub.PG2]
 
-# Configures Elixir's Logger
+
+# Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:user_id]
+
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- lib/ztd/application.ex
+++ lib/ztd/application.ex
@@ -1,8 +1,8 @@
 defmodule ZTD.Application do
   use Application
 
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
+
+  @doc "Start application supervision tree"
   def start(_type, _args) do
     import Supervisor.Spec
 
@@ -22,10 +22,12 @@ defmodule ZTD.Application do
     Supervisor.start_link(children, opts)
   end
 
+
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
   def config_change(changed, _new, removed) do
     ZTD.Web.Endpoint.config_change(changed, removed)
     :ok
   end
+
 end
